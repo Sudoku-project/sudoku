be2153c949605e136f58501755a7c1af9c4232af
Adrien <adrienlcp@gmail.com>
2022-04-05 14:55:35 +0200

    init project

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './style.scss';
+
+const Home = () => {
+
+  return (
+    <div className='home'>
+      Sudoku
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './style.scss';
+
+const NotFound = () => {
+
+  return (
+    <div className='not_found'>
+      
+    </div>
+  );
+};
+
+export default NotFound;
