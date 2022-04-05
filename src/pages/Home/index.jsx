c3ba33a71f66a667ccc51b8f59e07c8fb66cc2d8
Clement CHARLES <clement.charles@origins-digital.com>
2022-04-05 17:08:57 +0200

    addFooter

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import Grid from '../../components/Grid';
+import Grid from "../../components/Grid";
 
-import './style.scss';
+import "./style.scss";
 
 const Home = () => {
-
   return (
-    <div className='home'>
+    <div className="home">
       <Grid />
+      <footer> copyright Adrien & Clément </footer>
     </div>
   );
 };
