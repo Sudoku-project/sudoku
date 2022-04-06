78ca3064b60086864ac5953af071e2942e44d201
Adrien <adrienlcp@gmail.com>
2022-04-06 15:19:17 +0200

    set generate first line, init columns & squares

--- src/components/Grid/container.jsx
+++ src/components/Grid/container.jsx
@@ -4,6 +4,12 @@ import GridView from './view.jsx';
 const GridContainer = () => {
 
   const [grid, setGrid] = useState();
+  
+  const [squares, setSquares] = useState();
+
+  const [lines, setLines] = useState([]);
+
+  const [columns, setColumns] = useState([]);
 
   useEffect(() => {
     generateFirstRow();
@@ -16,75 +22,39 @@ const GridContainer = () => {
   const generateFirstRow = () => {
 
     // Init array for the first line of the grid
-    let firstLine = [];
-    
+    let linesArray = [[], [], [], [], [], [], [], [], []];
+    let squaresArray = [[], [], [], [], [], [], [], [], []];
+    let columnsArray = [[], [], [], [], [], [], [], [], []];
+
     // For 9 differents numbers,
     for (let i = 0; i < 9;)  {
       // We generate a new random number between 1 and 9
       const newNumber = randomNumber(1, 9);
 
       // If this random number isn't already in our first line,
-      if (!firstLine.includes(newNumber)) {
+      if (!linesArray[0].includes(newNumber)) {
         // We push it in our line
-        firstLine.push(newNumber);
+        linesArray[0].push(newNumber);
         // Then, we increase i
         i++;
       };
     };
+
+    // We push the three numbers for the right three squares
+    squaresArray[0].push([linesArray[0][0], linesArray[0][1], linesArray[0][2]]);
+    squaresArray[1].push([linesArray[0][3], linesArray[0][4], linesArray[0][5]]);
+    squaresArray[2].push([linesArray[0][6], linesArray[0][7], linesArray[0][8]]);
+
+   
     
-    // When our for loop is done, we generate second line
-    generateSecondRow(firstLine);
-  };
-
-  const generateSecondRow = (firstLine) => {
-    
-    let secondLine = [];
-
-    for (let i = 0; i < 9;) {
-
-      // We generate a new line of unique int between 1 and 9
-      const newNumber = randomNumber(1, 9);
-
-      // if my second line don't include my new number
-      if (!secondLine.includes(newNumber)) {
-          
-        for (let j = 0; j < 3;){
-          if (firstLine[0] === newNumber || firstLine[1] === newNumber || firstLine[2] === newNumber) {
-            return;
-          } else {
-            secondLine.push(newNumber);
-            i++;
-            j++;
-          };
-        };
-
-        for (let j = 0; j < 3;){
-          if (firstLine[3] === newNumber || firstLine[4] === newNumber || firstLine[5] === newNumber) {
-            return;
-          } else {
-            secondLine.push(newNumber);
-            i++;
-            j++;
-          };
-        };
-
-        for(let j = 0; j < 3;){
-          
-          if (firstLine[6] === newNumber || firstLine[7] === newNumber || firstLine[8] === newNumber) {
-            return;
-          } else {
-            secondLine.push(newNumber);
-            i++;
-            j++;
-          };
-        };
-
-      };
-
-      console.log('line 1 = ' + firstLine);
-      console.log('line 2 = ' + secondLine);
+    for (let i = 0; i < 9; i++) {
+      columnsArray[i].push(linesArray[0][i]);
     };
+    console.log('squaresArray = ', squaresArray)
+    console.log('colonnes = ', columnsArray);
+    console.log('lignes = ', linesArray);
   };
+
 
 	return (
     <>
